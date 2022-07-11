ad559bd1be260e24fadab4aed976039d0bededf3
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-07-11 16:48:48 -0600

    Creacion Del Proyecto

---
+++ JavaSQL/src/javasql/JavaSQL.java
@@ -0,0 +1,17 @@
+
+package javasql;
+
+/**
+ *
+ * @author Felipe Obando Arrieta
+ */
+public class JavaSQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
